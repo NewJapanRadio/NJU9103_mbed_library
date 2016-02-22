ad5054a8c75ccc85867ef40f156915f4e2e22b6d
abe.masamichi <m.abe@njr.co.jp>
2016-02-22 12:55:33 +0900

    Add define register values to header

--- src/NJU9103/NJU9103.h
+++ src/NJU9103/NJU9103.h
@@ -91,4 +91,181 @@ namespace NewJapanRadio {
     };
 }
 
+/**
+ * @defgroup REGISTER Register Values
+ * @{
+ */
+
+/**
+ * @defgroup CTRL_REG CTRL
+ * @{
+ */
+
+/**
+ * @name MODE[3:0]
+ * @{
+ */
+#define CTRL_MODE_IDLE                        0x0
+#define CTRL_MODE_SINGLE_CONVERSION           0x2
+#define CTRL_MODE_CONTINUOUS_CONVERSION       0x3
+#define CTRL_MODE_SINGLE_CHOP_CONVERSION      0x4
+#define CTRL_MODE_CONTINUOUS_CHOP_CONVERSION  0x5
+#define CTRL_MODE_INTERNAL_OFFSET_CALIBRATION 0x8
+#define CTRL_MODE_INTERNAL_GAIN_CALIBRATION   0x9
+#define CTRL_MODE_PGA_OFFSET_CALIBRATION      0xA
+#define CTRL_MODE_SYSTEM_OFFSET_CALIBRATION   0xC
+#define CTRL_MODE_SYSTEM_GAIN_CALIBRATION     0xD
+#define CTRL_MODE_BOOT                        0xF
+/* @} MODE[3:0] */
+
+/**
+ * @name CHSEL[5:4]
+ * @{
+ */
+#define CTRL_CHSEL_INP_INN  (0x0 << 4)
+#define CTRL_CHSEL_INP_GND  (0x2 << 4)
+#define CTRL_CHSEL_INN_GND  (0x3 << 4)
+#define CTRL_CHSEL_VREF_GND (0x4 << 4)
+#define CTRL_CHSEL_GND_GND  (0x5 << 4)
+#define CTRL_CHSEL_INN_INN  (0x6 << 4)
+/* @} CHSEL[5:4] */
+
+/**
+ * @name OV[6]
+ * @{
+ */
+#define CTRL_OV_NOERROR  (0x0 << 6)
+#define CTRL_OV_OVERFLOW (0x1 << 6)
+/* @} OV[6] */
+
+/**
+ * @name RDYB[7]
+ * @{
+ */
+#define CTRL_RDYB_READY (0x0 << 7)
+#define CTRL_RDYB_BUSY  (0x1 << 7)
+/* @} RDYB[7] */
+
+/* @} CTRL_REG */
+
+/**
+ * @defgroup PGACONF_REG PGACONF
+ * @{
+ */
+
+/**
+ * @name PGA1GAIN[2:0]
+ * @{
+ */
+#define PGACONF_PGA1GAIN_X1   0x0
+#define PGACONF_PGA1GAIN_X2   0x1
+#define PGACONF_PGA1GAIN_X4   0x2
+#define PGACONF_PGA1GAIN_X8   0x3
+#define PGACONF_PGA1GAIN_X16  0x4
+#define PGACONF_PGA1GAIN_X32  0x5
+#define PGACONF_PGA1GAIN_X64  0x6
+#define PGACONF_PGA1GAIN_X128 0x7
+/* @} PGA1GAIN[2:0] */
+
+/**
+ * @name PGA1EN[3]
+ * @{
+ */
+#define PGACONF_PGA1EN_DISABLE (0x0 << 3)
+#define PGACONF_PGA1EN_ENABLE  (0x1 << 3)
+/* @} PGA1EN[3] */
+
+/**
+ * @name PGA2GAIN[5:4]
+ * @{
+ */
+#define PGACONF_PGA2GAIN_X1   (0x0 << 4)
+#define PGACONF_PGA2GAIN_X2   (0x1 << 4)
+#define PGACONF_PGA2GAIN_X4   (0x2 << 4)
+/* @} PGA2GAIN[5:4] */
+
+/* @} PGACONF_REG */
+
+/**
+ * @defgroup CLKCONF_REG CLKCONF
+ * @{
+ */
+
+/**
+ * @name OSR[2:0]
+ * @{
+ */
+#define CLKCONF_OSR_64  0x0
+#define CLKCONF_OSR_128 0x1
+#define CLKCONF_OSR_256 0x2
+#define CLKCONF_OSR_512 0x3
+/* @} OSR[2:0] */
+
+/**
+ * @name CLKDIV[5:4]
+ * @{
+ */
+#define CLKCONF_CLKDIV_FOSC_2  (0x0 << 4)
+#define CLKCONF_CLKDIV_FOSC_4  (0x1 << 4)
+#define CLKCONF_CLKDIV_FOSC_8  (0x2 << 4)
+#define CLKCONF_CLKDIV_FOSC_16 (0x3 << 4)
+/* @} CLKDIV[5:4] */
+
+/* @} CLKCONF_REG */
+
+/**
+ * @defgroup DACCONF_REG DACCONF
+ * @{
+ */
+
+/**
+ * @name CALDAC[4:0]
+ * @{
+ */
+#define DACCONF_CALDAC_M16 0x10
+#define DACCONF_CALDAC_M15 0x11
+#define DACCONF_CALDAC_M14 0x12
+#define DACCONF_CALDAC_M13 0x13
+#define DACCONF_CALDAC_M12 0x14
+#define DACCONF_CALDAC_M11 0x15
+#define DACCONF_CALDAC_M10 0x16
+#define DACCONF_CALDAC_M9  0x17
+#define DACCONF_CALDAC_M8  0x18
+#define DACCONF_CALDAC_M7  0x19
+#define DACCONF_CALDAC_M6  0x1A
+#define DACCONF_CALDAC_M5  0x1B
+#define DACCONF_CALDAC_M4  0x1C
+#define DACCONF_CALDAC_M3  0x1D
+#define DACCONF_CALDAC_M2  0x1E
+#define DACCONF_CALDAC_M1  0x1F
+#define DACCONF_CALDAC_0   0x00
+#define DACCONF_CALDAC_1   0x01
+#define DACCONF_CALDAC_2   0x02
+#define DACCONF_CALDAC_3   0x03
+#define DACCONF_CALDAC_4   0x04
+#define DACCONF_CALDAC_5   0x05
+#define DACCONF_CALDAC_6   0x06
+#define DACCONF_CALDAC_7   0x07
+#define DACCONF_CALDAC_8   0x08
+#define DACCONF_CALDAC_9   0x09
+#define DACCONF_CALDAC_10  0x0A
+#define DACCONF_CALDAC_11  0x0B
+#define DACCONF_CALDAC_12  0x0C
+#define DACCONF_CALDAC_13  0x0D
+#define DACCONF_CALDAC_14  0x0E
+#define DACCONF_CALDAC_15  0x0F
+/* @} CALDAC[4:0] */
+
+/**
+ * @name CALDACEN[5]
+ * @{
+ */
+#define DACCONF_CALDACEN_DISABLE (0x0 << 5)
+#define DACCONF_CALDACEN_ENABLE  (0x1 << 5)
+/* @} CALDACEN[5] */
+
+/* @} DACCONF_REG */
+
+/* @} REGISTER */
+
 #endif // __NJU9103_H__
--- src/main.cpp
+++ src/main.cpp
@@ -16,11 +16,11 @@ int main() {
     int16_t adcdata;
     uint8_t wd[2] = { 0x00, 0x00 };
 
-    // CALDACEN:ON, CALDAC:-15
-    wd[0] = (1 << 5) | (-16 & 0x1F);
+    // CALDACEN:ON, CALDAC:-16
+    wd[0] = DACCONF_CALDACEN_ENABLE | DACCONF_CALDAC_M16;
     nju9103.write(NJU9103::DACCONF, wd, 1);
     // PGA1EN:ON, PGA1GAIN:32;
-    wd[0] = (1 << 4) | 5;
+    wd[0] = PGACONF_PGA1EN_ENABLE | PGACONF_PGA1GAIN_X32;
     nju9103.write(NJU9103::PGACONF, wd, 1);
 
     serial.printf("Run NJU9103\n");
